4ceabebe9c5a0da2b0d2959ffde2ff6cfc83be50
--unset <--unset>
2023-02-12 18:49:38 +0530

    deploying method 2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/google-timer/",
   plugins: [react()],
+  base: "/google-timer/",
 });
